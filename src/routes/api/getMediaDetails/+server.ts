f89fb0d1899b24a6276b346098f684bed124cad3
kyetbedford <128821036+kyetbedford@users.noreply.github.com>
2023-03-24 14:38:49 +0000

    Update +server.ts

--- src/routes/api/getMediaDetails/+server.ts
+++ src/routes/api/getMediaDetails/+server.ts
@@ -3,7 +3,7 @@ import { json } from '@sveltejs/kit';
 
 export async function POST({ request }: { request: any }) {
 	const { title } = await request.json();
-	const url = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${title}`;
+	const url = `http://www.omdbapi.com/?apikey=$a52dde98&t=${title}`;
 
 	const res = await fetch(url);
 	const details = await res.json();
